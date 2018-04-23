187bc4859a56ca1d3d6a330a3fe0c18c19712524
Your Name <esheen11@gmail.com>
2018-04-23 13:23:24 -0400

    removed folder

--- word_count/try.js
+++
@@ -1,5 +0,0 @@
-/*
-Just draw a border round the document.body.
-*/
-// var textbox_id = document.createElement("INPUT");
-// document.body.textContent.split(document.getElementById('textbox_id').value).length
