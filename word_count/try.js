6ae3d23e49c460bb0f69ea27fcbd199273d137eb
erip <25800139+erip1@users.noreply.github.com>
2018-04-23 13:21:53 -0400

    Add files via upload

---
+++ word_count/try.js
@@ -0,0 +1,5 @@
+/*
+Just draw a border round the document.body.
+*/
+// var textbox_id = document.createElement("INPUT");
+// document.body.textContent.split(document.getElementById('textbox_id').value).length
